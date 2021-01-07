c5128c2b9c7f7a096be14befb24619d8d98cb618
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-01-07 20:37:03 +0100

    Removed background; Replaced with terminal colour

--- home/awesome/rc.lua
+++ home/awesome/rc.lua
@@ -192,16 +192,19 @@ local tasklist_buttons = gears.table.join(
                                           end))
 
 --]=====]
+
 local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
-    end
+    gears.wallpaper.set("#1D1F21")
+    -- Uncomment this to use the themes wallpaper
+    -- -- Wallpaper
+    -- if beautiful.wallpaper then
+    --     local wallpaper = beautiful.wallpaper
+    --     -- If wallpaper is a function, call it with the screen
+    --     if type(wallpaper) == "function" then
+    --         wallpaper = wallpaper(s)
+    --     end
+    --     gears.wallpaper.maximized(wallpaper, s, true)
+    -- end
 end
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
