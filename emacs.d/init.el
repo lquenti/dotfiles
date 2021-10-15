751540dfae663ffba3038c2697854aac3d0181ac
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-10-15 22:48:51 +0200

    :construction: WIP initial init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,24 +2,65 @@
 (setq initial-scratch-message ";; happy hacking")
 (scroll-bar-mode -1)
 
-;; no blinking in this house
+;; no bells or blinking
+(setq visible-bell nil ring-bell-function 'ignore)
+
+;; I said no blinking!
 (blink-cursor-mode -1)
 
 ;; Once I get better I'll remove it
 ;; For now it's quite useful
-;; (tool-bar-mode -1) ;; Toolbar are big icons
-;; (menu-bar-mode -1) ;; the smol bar
+; (tool-bar-mode -1) ;; Toolbar are big icons
+; (menu-bar-mode -1) ;; the smol bar
 
 ;; Frame title
-;; TODO: What is the first empty string for
-(setq frame-title-format '("" "emacs - Creating RSI's since 1976"))
+(setq frame-title-format '("" "emacs schmemacs sag ich ma"))
 
 ;; Don't trash everything with autosaves
-;; TODO: Again, what is the noise
 (setq backup-directory-alist '(("." . "/tmp/emacsbackups")))
 
 ;; TODO: Add linum-relative for absolute relative line numbers
 (global-display-line-numbers-mode)
 
 ;; Show where to aggregate my agenda from
-(setq org-agenda-files '("~/Documents/org/todo.org"))
+(setq org-agenda-files (directory-files-recursively "~/Documents/org/" "\\.org$"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Bootstrap use-package
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+
+;; Repoinit. Analagous to apt update
+(unless package-archive-contents (package-refresh-contents))
+
+;; <SOMETHING>-p => its a predicate <=> a -> Bool
+(unless (package-installed-p 'use-package) (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+;; Make ESC quit prompts
+;; Normally it would take ESC-ESC-ESC
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Font Size
+(set-face-attribute 'default nil :height 110)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("/home/lquenti/Documents/org/todo.org")))
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
