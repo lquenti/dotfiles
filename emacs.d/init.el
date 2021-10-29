be5566f7a415e929ebb51512730d13b191ec047b
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-10-30 00:02:21 +0200

    :art: Changed Font in Emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -47,20 +47,9 @@
 ;; Normally it would take ESC-ESC-ESC
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-;; Font Size
-(set-face-attribute 'default nil :height 110)
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("/home/lquenti/Documents/org/todo.org")))
- '(package-selected-packages (quote (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Iosevka Term" :foundry "CYEL" :slant normal :weight normal :height 98 :width normal)))))
