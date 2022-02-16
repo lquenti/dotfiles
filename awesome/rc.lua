35672853e44f9d709786b82aa16e48dc5e9f479e
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-02-16 15:31:49 +0100

    :bug: fix collision of keymaps

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -247,7 +247,7 @@ root.buttons(gears.table.join(
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
-    awful.key({ modkey,           }, "l", function () awful.util.spawn("xscreensaver-command -l") end, {description="Spawn xscreensaver"}),
+    awful.key({ modkey,           }, "F12", function () awful.util.spawn("xscreensaver-command -l") end, {description="Spawn xscreensaver"}),
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
