905ecb85a2b6a6b7ff40458edc7daf96f8d60ea6
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-04-09 19:32:49 +0200

    :sparkles: battery widget

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -229,6 +229,7 @@ awful.screen.connect_for_each_screen(function(s)
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
+            require("battery-widget") {},
             wibox.widget.systray(),
             mytextclock,
             s.mylayoutbox,
