145e846e1a9859b5f7724a689d0f89eb7cd2cd93
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-05-03 10:01:36 +0200

    docker install

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@
 #   - [ ] install i3
 # - [ ] install lazygit
 # - [ ] install nvm + node
+# - [x] install docker
 
 PATH_TO_SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -60,7 +61,42 @@ install_apt_packages() {
     feh
 }
 
+install_docker() {
+  # See: https://docs.docker.com/engine/install/ubuntu/
+
+  # let us only do this if docker is not installed since
+  # this is not idempotent by default
+  if [ -x "$(command -v docker)" ]; then
+    echo "Docker is already installed"
+    return
+  fi
+
+  sudo apt-get update
+  sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  newgrp docker
+}
+
 ubuntu_or_die
-# Do the sudo work first
+
+# Install global stuff
 install_apt_packages
+install_docker
+
+# Do user space stuff
 install_rust
