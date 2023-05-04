f5bb959d20be536716eed00be59eca4e5f7da636
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-05-04 10:10:38 +0200

    screenshot + toggle touchpad activity

--- i3/launch.sh
+++ i3/launch.sh
@@ -3,3 +3,18 @@
 # Keyboard layout stuff
 setxkbmap us
 setxkbmap -option caps:escape
+
+# stuff
+flameshot &
+
+# Toggle touch with ESC
+toggle_touchpad() {
+  TOUCHPAD_ID=$(xinput list | grep Touchpad | grep -o 'id=[0-9]*' | sed 's/id=//')
+  ACTIVE=$(xinput list-props 11 | grep -i enabled | grep -o ".$")
+  if [ $ACTIVE -eq 1 ]; then
+    xinput --disable $TOUCHPAD_ID
+  else
+    xinput --enable $TOUCHPAD_ID
+  fi
+}
+# TODO bind to key
