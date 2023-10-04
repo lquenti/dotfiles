76b23f7c429dd477079b66936188fe6f220665d4
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-10-04 13:27:56 +0200

    remove telescope, add nvim tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -177,27 +177,6 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
-  -- Fuzzy Finder (files, lsp, etc)
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-    },
-  },
-
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
@@ -206,6 +185,11 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+
+  -- Nvim tree
+  {
+    'kyazdani42/nvim-tree.lua'
+  }
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
@@ -233,8 +217,8 @@ vim.o.hlsearch = true
 vim.o.cursorline = false
 
 -- Minimal number of lines to keep above and below cursor
-vim.opt.scrolloff = 4
-vim.opt.sidescrolloff = 4
+vim.o.scrolloff = 4
+vim.o.sidescrolloff = 4
 
 -- Make absolute relative line numbers default
 vim.wo.number = true
@@ -285,41 +269,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
-
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
-
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
-
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
@@ -548,6 +497,43 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+-- configure nvim tree
+require("nvim-tree").setup({
+  renderer = {
+    icons = {
+      web_devicons = {
+        file = {
+          enable = false,
+          color = false,
+        },
+        folder = {
+          enable = false,
+          color = false,
+        }
+      },
+      glyphs = {
+        default = "-", -- is a file
+        symlink = "-", -- is a symlink
+        bookmark = "b", -- is a bookmark?
+        modified = "●",
+        folder = {
+          arrow_closed = "-", -- collapsed
+          arrow_open = "-", -- not collapsed
+          default = "/", -- is a normal folder
+          open = "/", -- is a non-collapsed folder
+          empty = "/", -- ...
+          empty_open = "/",
+          symlink = "/",
+          symlink_open = "/",
+        }
+      }
+    }
+  }
+})
+
+--vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>", {noremap = true })
+vim.keymap.set("n", "<C-x><C-f>", ":NvimTreeToggle<cr>", {noremap = true})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
